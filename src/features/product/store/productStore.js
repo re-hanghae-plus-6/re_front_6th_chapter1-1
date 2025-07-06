eb03e01bcd0b92a931292c89765e61009d1b975a
YeongseoYoon <l112xpla@gmail.com>
2025-07-06 20:04:26 +0900

    feat: productStore구현

---
+++ src/features/product/store/productStore.js
@@ -0,0 +1,23 @@
+import { createState } from "../../../utils/createState.js";
+
+export const productStore = createState({
+  products: [],
+  categories: [],
+  loading: false,
+  pagination: {
+    page: 1,
+    limit: 20,
+    total: 0,
+    totalPages: 1,
+    hasNext: false,
+    hasPrev: false,
+  },
+  params: {
+    page: 1,
+    limit: 20,
+    search: "",
+    category1: "",
+    category2: "",
+    sort: "price_asc",
+  },
+});
