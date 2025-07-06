f249d67de62787f6f747eb27103a0f23ab4b78ae
jsheo <tjrwjdgj@gmail.com>
2025-07-07 01:17:12 +0900

    test: 검색 및 카테고리 적용 전 테스트 코드
    카테고리 적용 전 상태, 추후 컴포넌트 분리 표시

--- src/eg-templates.js
+++ src/eg-templates.js
@@ -1,4 +1,4 @@
-const 상품목록_레이아웃_로딩 = `
+const 상품목록_레이아웃_로딩 = /*html*/ `
     <div class="min-h-screen bg-gray-50">
       <header class="bg-white shadow-sm sticky top-0 z-40">
         <div class="max-w-md mx-auto px-4 py-4">
@@ -148,7 +148,7 @@ const 상품목록_레이아웃_로딩 = `
     </div>
   `;
 
-const 상품목록_레이아웃_로딩완료 = `
+const 상품목록_레이아웃_로딩완료 = /*html*/ `
     <div class="bg-gray-50">
       <header class="bg-white shadow-sm sticky top-0 z-40">
         <div class="max-w-md mx-auto px-4 py-4">
@@ -321,7 +321,7 @@ const 상품목록_레이아웃_로딩완료 = `
     </div>
   `;
 
-const 상품목록_레이아웃_카테고리_1Depth = `
+const 상품목록_레이아웃_카테고리_1Depth = /*html*/ `
     <main class="max-w-md mx-auto px-4 py-4">
       <!-- 검색 및 필터 -->
       <div class="bg-white rounded-lg shadow-sm border border-gray-200 p-4 mb-4">
@@ -401,7 +401,7 @@ const 상품목록_레이아웃_카테고리_1Depth = `
     </main>
   `;
 
-const 상품목록_레이아웃_카테고리_2Depth = `
+const 상품목록_레이아웃_카테고리_2Depth = /*html*/ `
     <main class="max-w-md mx-auto px-4 py-4">
       <!-- 검색 및 필터 -->
       <div class="bg-white rounded-lg shadow-sm border border-gray-200 p-4 mb-4">
@@ -481,7 +481,7 @@ const 상품목록_레이아웃_카테고리_2Depth = `
     </main>
   `;
 
-const 토스트 = `
+const 토스트 = /*html*/ `
     <div class="flex flex-col gap-2 items-center justify-center mx-auto" style="width: fit-content;">
       <div class="bg-green-600 text-white px-4 py-3 rounded-lg shadow-lg flex items-center space-x-2 max-w-sm">
         <div class="flex-shrink-0">
@@ -527,7 +527,7 @@ const 토스트 = `
     </div>
   `;
 
-const 장바구니_비어있음 = `
+const 장바구니_비어있음 = /*html*/ `
     <div class="flex min-h-full items-end justify-center p-0 sm:items-center sm:p-4">
       <div class="relative bg-white rounded-t-lg sm:rounded-lg shadow-xl w-full max-w-md sm:max-w-lg max-h-[90vh] overflow-hidden">
         <!-- 헤더 -->
@@ -565,7 +565,7 @@ const 장바구니_비어있음 = `
     </div>
   `;
 
-const 장바구니_선택없음 = `
+const 장바구니_선택없음 = /*html*/ `
     <div class="flex min-h-full items-end justify-center p-0 sm:items-center sm:p-4">
       <div class="relative bg-white rounded-t-lg sm:rounded-lg shadow-xl w-full max-w-md sm:max-w-lg max-h-[90vh] overflow-hidden">
         <!-- 헤더 -->
@@ -716,7 +716,7 @@ const 장바구니_선택없음 = `
     </div>
   `;
 
-const 장바구니_선택있음 = `
+const 장바구니_선택있음 = /*html*/ `
     <div class="flex min-h-full items-end justify-center p-0 sm:items-center sm:p-4">
       <div class="relative bg-white rounded-t-lg sm:rounded-lg shadow-xl w-full max-w-md sm:max-w-lg max-h-[90vh] overflow-hidden">
         <!-- 헤더 -->
@@ -875,7 +875,7 @@ const 장바구니_선택있음 = `
     </div>
   `;
 
-const 상세페이지_로딩 = `
+const 상세페이지_로딩 = /*html*/ `
     <div class="min-h-screen bg-gray-50">
       <header class="bg-white shadow-sm sticky top-0 z-40">
         <div class="max-w-md mx-auto px-4 py-4">
@@ -915,7 +915,7 @@ const 상세페이지_로딩 = `
     </div>
   `;
 
-const 상세페이지_로딩완료 = `
+const 상세페이지_로딩완료 = /*html*/ `
     <div class="min-h-screen bg-gray-50">
       <header class="bg-white shadow-sm sticky top-0 z-40">
         <div class="max-w-md mx-auto px-4 py-4">
@@ -1076,7 +1076,7 @@ const 상세페이지_로딩완료 = `
     </div>
   `;
 
-const _404_ = `
+const _404_ = /*html*/ `
     <main class="max-w-md mx-auto px-4 py-4">
       <div class="text-center my-4 py-20 shadow-md p-6 bg-white rounded-lg">
       <svg viewBox="0 0 320 180" xmlns="http://www.w3.org/2000/svg">
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { home } from "./pages/home.js";
+import { getProducts } from "./api/productApi.js";
+import { getCategories } from "./api/productApi.js";
 
 const enableMocking = () =>
   import("./mocks/browser.js").then(({ worker }) =>
@@ -7,8 +9,33 @@ const enableMocking = () =>
     }),
   );
 
-function main() {
-  document.body.querySelector("#root").innerHTML = home();
+/**
+ * 메인 홈페이지 렌더링 및 상태 값
+ */
+const statusHome = {
+  products: [],
+  total: 0,
+  loading: false,
+  categories: [],
+};
+async function initCategories() {
+  statusHome.categories = await getCategories();
+  console.log(statusHome.categories);
+}
+function homeRender() {
+  document.body.querySelector("#root").innerHTML = home(statusHome);
+}
+async function initHome() {
+  let data = await getProducts({});
+  statusHome.products = data.products;
+  statusHome.total = data.pagination.total;
+}
+async function main() {
+  statusHome.loading = true;
+  homeRender(statusHome);
+  await Promise.all([initHome(), initCategories()]);
+  statusHome.loading = false;
+  homeRender(statusHome);
 }
 
 // 애플리케이션 시작
