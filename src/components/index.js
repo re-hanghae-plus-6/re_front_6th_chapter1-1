04e797ca6fec983a19fdc71c52775a90c5d52635
devchangjun <ckdwns9121@naver.com>
2025-07-06 14:26:07 +0900

    feat: 컴포넌트 통합 export용 index.js 파일 생성

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+// Layout Components
+export { default as Header } from "./layout/Header.js";
+export { default as Footer } from "./layout/Footer.js";
+export { default as MainLayout } from "./layout/MainLayout.js";
+
+// Loading Components
+export { default as Loading } from "./ui/Loading.js";
+export { LoadingSpinner } from "./ui/Loading.js";
+export { default as ProductSkeleton } from "./product/ProductSkeleton.js";
+
+// Product Components
+export { default as ProductItem } from "./product/ProductItem.js";
+export { default as ProductGrid } from "./product/ProductGrid.js";
+export { default as ProductDetailPage } from "./page/ProductDetailPage.js";
+export { default as RelatedProducts } from "./product/RelatedProducts.js";
+
+// Filter Components
+export { default as SearchInput } from "./filter/SearchInput.js";
+export { default as Category } from "./filter/Category.js";
+export { default as SortFilter } from "./filter/SortFilter.js";
+export { default as LimitFilter } from "./filter/LimitFilter.js";
+export { default as FilterSection } from "./filter/FilterSection.js";
+
+// Cart Components
+export { default as CartModal } from "./cart/CartModal.js";
+export { default as CartItem } from "./cart/CartItem.js";
+export { default as CartEmpty } from "./cart/CartEmpty.js";
+
+// UI Components
+export { default as Toast } from "./ui/Toast.js";
+export { ToastContainer } from "./ui/Toast.js";
+export { default as Breadcrumb } from "./ui/Breadcrumb.js";
+export { default as NotFoundPage } from "./page/NotFoundPage.js";
