5ef6a3c923bc4bd0c3c03d7da2dd20e449c042ea
tksemf7410 <tksemf7410@gmail.com>
2025-07-07 03:47:07 +0900

    fix: navigation 컴포넌트 props 수정

--- src/components/productList/layout/navigation.js
+++ src/components/productList/layout/navigation.js
@@ -1,9 +1,9 @@
-export const Navigation = ({ cartItemCount = 0 }) => `
+export const Navigation = ({ title, cartItemCount = 0 }) => `
  <header class="bg-white shadow-sm sticky top-0 z-40">
         <div class="max-w-md mx-auto px-4 py-4">
           <div class="flex items-center justify-between">
             <h1 class="text-xl font-bold text-gray-900">
-              <a href="/" data-link="">쇼핑몰</a>
+              <a href="/" data-link="">${title}</a>
             </h1>
             <div class="flex items-center space-x-2">
               <!-- 장바구니 아이콘 -->
