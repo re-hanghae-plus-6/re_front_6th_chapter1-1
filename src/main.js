3f33a443549db70809e4ddcacafb138ed0922b6a
YeongseoYoon <l112xpla@gmail.com>
2025-07-06 16:52:01 +0900

    feat: data-link인 경우 html문서를 다시 불러오지 않도록 처리

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createRouter, router } from "./router.js";
 import { render } from "./render.js";
+import { addEvent } from "./utils/eventManager.js";
 import { ProductListPage } from "./pages/ProductListPage.js";
 import { ProductDetailPage } from "./pages/ProductDetailPage.js";
 
@@ -9,6 +10,11 @@ const enableMocking = () =>
       onUnhandledRequest: "bypass",
     }),
   );
+
+addEvent("click", "[data-link]", (e) => {
+  e.preventDefault();
+  router.get().push(e.target.href.replace(window.location.origin, ""));
+});
 
 const routes = {
   "/": ProductListPage,
