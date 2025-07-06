3635a44b808c1b608eca8e306d4bca2cdba01ecf
tksemf7410 <tksemf7410@gmail.com>
2025-07-07 01:53:40 +0900

    refactor: 분리한 컴포넌트로 페이지 재구성한 예시 제작

---
+++ src/components/productList/pages/상품목록_레이아웃_로딩.js
@@ -0,0 +1,32 @@
+import { Navigation } from "./layout/navigation.js";
+import { SkeletonProduct } from "./skeleton/skeletonProduct.js";
+import { LoadingBar } from "./layout/loadingBar.js";
+import { Footer } from "./layout/footer.js";
+import { ProductGrid } from "./layout/productGrid.js";
+import { Category } from "./category/category.js";
+
+export const 상품목록_레이아웃_로딩 = `
+<div class="min-h-screen bg-gray-50">
+  ${Navigation({ cartItemCount: 6 })}
+  <main class="max-w-md mx-auto px-4 py-4">
+  ${Category()}
+    <!-- 상품 목록 -->
+    <div class="mb-6">
+      <div>
+        <!-- 상품 그리드 -->
+        ${ProductGrid({
+          children: `
+          <!-- 로딩 스켈레톤 -->
+          ${SkeletonProduct}
+          ${SkeletonProduct}
+          ${SkeletonProduct}
+          ${SkeletonProduct}
+          `,
+        })}
+        </div>
+        ${LoadingBar}
+    </div>
+  </main>
+  ${Footer}
+</div>
+`;
