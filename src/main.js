d4269bae963feeccb2c9614f6d2d32afdcbb88b9
heojungseok <heojungseok@heojungseokui-MacBookAir.local>
2025-07-07 22:56:18 +0900

    fix: 상품 목록 로딩 1-1 수정
    페이지가 그려지기 전 함수 호출 문제
    테스트 시 screen.logTestingPlaygroundURL() 활용

--- src/main.js
+++ src/main.js
@@ -19,45 +19,49 @@ const statusHome = {
   loading: false,
   params: {
     limit: 20,
-  }
+  },
+  categories: {},
 };
 // 카테고리 객체
-let categories = {}; 
-// 
 async function initCategories() {
-  categories = await getCategories();
+  statusHome.categories = await getCategories();
+}
+async function initList() {
+  let data = await getProducts({});
+  statusHome.products = data.products;
+  statusHome.total = data.pagination.total;
 }
 // 홈 렌더링
 function homeRender() {
   document.body.querySelector("#root").innerHTML = home(statusHome);
 }
 // 카테고리 렌더링
-function categoriesRender(categoriesData) {
-    const filterCompEl = document.body.querySelector("#filterComp");
-    if (filterCompEl) {
-      filterCompEl.innerHTML = searchNcategoriesComp(categoriesData, statusHome.loading);
-    }
+function categoriesRender(statusHome) {
+  const filterCompEl = document.body.querySelector("#filterComp");
+  console.log(filterCompEl);
+
+  if (filterCompEl) {
+    filterCompEl.innerHTML = searchNcategoriesComp(statusHome);
+  }
 }
 // API 호출 및 렌더링을 담당하는 함수
 async function loadProductsAndUpdateUI() {
   statusHome.loading = true;
   homeRender(); // 로딩 상태의 UI를 먼저 렌더링
-  categoriesRender(categories);
-
-  const data = await getProducts(statusHome.params);
-  statusHome.products = data.products;
-  statusHome.total = data.pagination.total;
-
+  // 카테고리 로드
+  await initCategories();
+  categoriesRender(statusHome);
+  await initList();
   statusHome.loading = false;
   homeRender(); // 데이터 로딩 후 다시 렌더링
-  categoriesRender(categories);
+  categoriesRender(statusHome);
 }
 // 이벤트 핸들러 설정 함수
 function setupEventListeners() {
   const filterCompEl = document.body.querySelector("#filterComp");
   if (filterCompEl) {
     filterCompEl.addEventListener("change", (e) => {
-      if(e.target.id === "limit-select") {
+      if (e.target.id === "limit-select") {
         const newLimit = parseInt(e.target.value, 10);
         statusHome.params.limit = newList;
         loadProductsAndUpdateUI();
@@ -65,17 +69,13 @@ function setupEventListeners() {
     });
   }
   // TODO: 그 외 다른 이벤트 설정.....
-  
 }
 
 async function main() {
-  // 카테고리 로드
-  await initCategories();
   // 상품 데이터 로드
   await loadProductsAndUpdateUI();
   // 이벤트 리스너 설정
   setupEventListeners();
-  
 }
 
 // 애플리케이션 시작
