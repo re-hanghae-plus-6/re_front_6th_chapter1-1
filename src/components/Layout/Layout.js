0b5c7d6f0584caf87c0e003cd77ef871ca753a3a
nimusmix <nimusmix@gmail.com>
2025-07-07 21:39:15 +0900

    style: 레이아웃 변경

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,9 @@ import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
 
 export const Layout = (content) => /* html */ `
+<div class="min-h-screen bg-gray-50">
   ${Navbar()}
   <main>${content}</main>
-  ${Footer}
+  ${Footer()}
+</div>
 `;
