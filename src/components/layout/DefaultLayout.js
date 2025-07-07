4841b3ce271653d1e8d9372e87d58805d1bd0fd8
sangsoo <wsckm1@gmail.com>
2025-07-07 18:35:21 +0900

    feat: layout header/footer 추가

--- src/components/layout/DefaultLayout.js
+++ src/components/layout/DefaultLayout.js
@@ -1,7 +1,12 @@
+import footer from './footer/Footer.js';
+import header from './header/Header.js';
+
 function DefaultLayout({ children }) {
   return `
     <div class="min-h-screen bg-gray-50">
+      ${header()}
       ${children}
+      ${footer()}
     </div>
   `;
 }
