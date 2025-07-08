e0c8e66509963ca13d8378f7d705daa0e4b544e4
YeongseoYoon <l112xpla@gmail.com>
2025-07-08 21:23:16 +0900

    feat: 장바구니 상태 관리 기능 추가

---
+++ src/shared/store/cartStore.js
@@ -0,0 +1,35 @@
+import { createState } from "../../utils/createState.js";
+import { loadFromStorage } from "../../utils/localStorage.js";
+
+const CART_STORAGE_KEY = "shopping_cart";
+
+const initialCartState = loadFromStorage(CART_STORAGE_KEY, {
+  items: [],
+  selectedItems: [],
+  totalCount: 0,
+  isModalOpen: false,
+});
+
+export const cartStore = createState(initialCartState);
+
+export const getCartState = () => cartStore.getState();
+
+export const updateCartState = (cartState) => {
+  cartStore.setState({
+    ...getCartState(),
+    ...cartState,
+  });
+};
+
+export const subscribeToCartState = (callback) => {
+  return cartStore.subscribe(callback);
+};
+
+export const resetCartState = () => {
+  cartStore.setState({
+    items: [],
+    selectedItems: [],
+    totalCount: 0,
+    isModalOpen: false,
+  });
+};
