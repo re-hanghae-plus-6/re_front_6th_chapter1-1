ca0fbeb8d0767dbace223f76a329339be9b464da
tksemf7410 <tksemf7410@gmail.com>
2025-07-08 13:40:27 +0900

    refactor: breadcumb 컴포넌트 리팩토링

--- src/components/productList/category/breadcrumb.js
+++ src/components/productList/category/breadcrumb.js
@@ -1,6 +1,22 @@
 export const Breadcrumb = ({ category1 = "", category2 = "" }) => `
     <div class="flex items-center gap-2">
         <label class="text-sm text-gray-600">카테고리:</label>
-        <button data-breadcrumb="reset" class="text-xs hover:text-blue-800 hover:underline">전체</button><span class="text-xs text-gray-500">&gt;</span><button data-breadcrumb="category1" data-category1="${category1}" class="text-xs hover:text-blue-800 hover:underline">${category1}</button><span class="text-xs text-gray-500">&gt;</span><span class="text-xs text-gray-600 cursor-default">${category2}</span>
+        <button data-breadcrumb="reset" class="text-xs hover:text-blue-800 hover:underline">전체</button>
+        ${
+          category1
+            ? `
+        <span class="text-xs text-gray-500">&gt;</span>
+        <button data-breadcrumb="category1" data-category1="${category1}" class="text-xs hover:text-blue-800 hover:underline">${category1}</button>
+        `
+            : ""
+        }
+        ${
+          category2
+            ? `
+        <span class="text-xs text-gray-500">&gt;</span>
+        <span class="text-xs text-gray-600 cursor-default">${category2}</span>
+        `
+            : ""
+        }
     </div>
 `;
