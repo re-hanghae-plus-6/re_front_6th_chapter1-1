15dae98a46246a932f463004dfd47b5239b9a8a7
sangsoo <wsckm1@gmail.com>
2025-07-08 18:45:16 +0900

    feat: 상품/카테고리 데이터 패칭 및 컴포넌트에 props로 전달

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,21 +1,47 @@
 import Component from '../../core/Component.js';
 import ProductContainer from './components/product/product-container/ProductContainer.js';
+import FilterContainer from './components/filter/filter-container/FilterContainer.js';
+import { getCategories, getProducts } from '../../api/productApi.js';
 
 class HomePage extends Component {
   constructor(element, props) {
     super(element, props);
+    this.state = {
+      loading: true,
+      products: null,
+      categories: null,
+    };
+  }
+
+  async onMount() {
+    const [products, categories] = await Promise.all([getProducts(), getCategories()]);
+    this.setState({
+      loading: false,
+      products,
+      categories,
+    });
   }
 
   render() {
     this.element.innerHTML = /* HTML */ `
       <main class="max-w-md mx-auto px-4 py-4">
+     
+        <div id="filter-container"></div>
+      
         <!-- 상품 목록 -->
         <div class="mb-6">
           <div id="product-container"></div>
       </main>
     `;
 
-    new ProductContainer(this.element.querySelector('#product-container')).mount();
+    new FilterContainer(this.element.querySelector('#filter-container'), {
+      loading: this.state.loading,
+      categories: this.state.categories,
+    }).mount();
+    new ProductContainer(this.element.querySelector('#product-container'), {
+      loading: this.state.loading,
+      products: this.state.products,
+    }).mount();
   }
 }
 
