3e0e52c2e33e5b7cd4b389677bf135f1fc105f78
Dan <44767362+geonhwiii@users.noreply.github.com>
2025-07-09 02:42:22 +0900

    fix: 비동기 상태 핸들링을 위해 "페이지당 상품 수 선택" 테스트 코드 수정

--- src/__tests__/product.list.test.js
+++ src/__tests__/product.list.test.js
@@ -102,16 +102,8 @@ describe("3. 페이지당 상품 수 선택", () => {
     const limitSelect = document.querySelector("#limit-select");
     await userEvent.selectOptions(limitSelect, "10");
 
-    await waitFor(() =>
-      expect(
-        screen.queryByRole("heading", {
-          level: 3,
-          name: "창틀벌레 모풍지판 창문 벌레 차단 틈새 창문틈 막이 방충망",
-        }),
-      ).not.toBeInTheDocument(),
-    );
-
-    expect(document.querySelectorAll(".product-card").length).toBe(10);
+    // ? 비동기 상태 핸들링을 위해 테스트 코드 수정
+    await waitFor(() => expect(document.querySelectorAll(".product-card").length).toBe(10));
   });
 });
 
