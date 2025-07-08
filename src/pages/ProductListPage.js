a86bd5dee7ad3d5b37bad49d2c5a3f55197a9d03
chan9yu <dev.cgyeo@gmail.com>
2025-07-09 00:09:10 +0900

    feat: icon 컴포넌트 분리

--- src/pages/ProductListPage.js
+++ src/pages/ProductListPage.js
@@ -1,4 +1,5 @@
 import { getCategories, getProducts } from "../api/productApi";
+import { SearchIcon } from "../components/icons/SearchIcon";
 import { Footer } from "../components/layouts/Footer";
 import { Header } from "../components/layouts/Header";
 import { ProductCard } from "../components/productList/ProductCard";
@@ -157,16 +158,7 @@ export class ProductListPage extends Component {
                   value=""
                   class="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                 />
-                <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                  <svg class="h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    ></path>
-                  </svg>
-                </div>
+                <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">${SearchIcon()}</div>
               </div>
             </div>
 
