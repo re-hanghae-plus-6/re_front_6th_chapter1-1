44def3798b8c874159519fb5d0814c7d29d8d44a
YeongseoYoon <l112xpla@gmail.com>
2025-07-09 19:13:47 +0900

    feat: CartStore구현

--- src/features/cart/store/cartStore.js
+++ src/features/cart/store/cartStore.js
@@ -1,7 +1,7 @@
 import { createState } from "../../../utils/createState.js";
 import { loadFromStorage } from "../../../utils/localStorage.js";
 
-const CART_STORAGE_KEY = "shopping_cart";
+export const CART_STORAGE_KEY = "shopping_cart";
 
 const initialCartState = loadFromStorage(CART_STORAGE_KEY, {
   items: [],
@@ -11,25 +11,3 @@ const initialCartState = loadFromStorage(CART_STORAGE_KEY, {
 });
 
 export const cartStore = createState(initialCartState);
-
-export const getCartState = () => cartStore.getState();
-
-export const updateCartState = (cartState) => {
-  cartStore.setState({
-    ...getCartState(),
-    ...cartState,
-  });
-};
-
-export const subscribeToCartState = (callback) => {
-  return cartStore.subscribe(callback);
-};
-
-export const resetCartState = () => {
-  cartStore.setState({
-    items: [],
-    selectedItems: [],
-    totalCount: 0,
-    isModalOpen: false,
-  });
-};
