169496b8229ebd9ebc1ede734c8e4d021beae7e6
Jung MyungHun <gncnzkf3@naver.com>
2025-07-10 01:39:23 +0900

    컴포넌트 분리

---
+++ src/pages/CartPage.js
@@ -0,0 +1,7 @@
+export const CartPage = () => {
+  return `
+    <main class="max-w-md mx-auto px-4 py-4">
+      장바구니페이지
+    </main>
+  `;
+};
---
+++ src/pages/ProductListPage.js
@@ -0,0 +1,7 @@
+export const ProductListPage = () => {
+  return `
+    <main class="max-w-md mx-auto px-4 py-4">
+      리스트페이지
+    </main>
+  `;
+};
