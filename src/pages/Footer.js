b4c12cd0053e05803cb218de32f2d2bfc553c884
이태영 <lhlhgodz3@innogird.com>
2025-07-09 19:52:47 +0900

    fix: Footer 문구 변경

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -2,7 +2,7 @@ export const Footer = () => {
   return /*html*/ `
     <footer class="bg-white shadow-md mt-8">
       <div class="max-w-md mx-auto px-4 py-6 text-center text-sm text-gray-600">
-        © 2023 쇼핑몰. All rights reserved.
+        © 2025 항해플러스 프론트엔드 6기 개발자 이태영 <br> All rights reserved.
       </div>
     </footer>
   `;
