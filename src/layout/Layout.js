584c82c26fe1863b42510662ab19471a118cb37b
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-09 14:31:39 +0900

    feat: 레이아웃 추가
    레이아웃, 헤더, 푸터 컴포넌트 추가

---
+++ src/layout/Layout.js
@@ -0,0 +1,11 @@
+import { Component } from "../core/Component";
+import { html } from "../utils/html";
+
+export class Layouy extends Component {
+  renderContainer() {
+    const { header, main, footer } = this.props;
+    return html` <div ${this.dataAttribute.attribute} class="min-h-screen bg-gray-50">
+      ${header} ${main} ${footer}
+    </div>`;
+  }
+}
