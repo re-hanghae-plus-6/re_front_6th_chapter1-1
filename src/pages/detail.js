f7caac12a1eb3679c2477a5b674777463c5e8b56
hmyo2853 <hmyo2853@gmail.com>
2025-07-11 01:24:30 +0900

    Feature : 상품 상세 페이지 추가

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,3 +1,38 @@
-export default function DetailPage() {
-  return /* HTML */ ` <div>상세페이지</div> `;
+import { getProduct } from "../api/productApi.js";
+// import { DetailStore } from "../store.js";
+import { DetailHeader } from "../components/header.js";
+import Footer from "../components/footer.js";
+import ProductDetail from "../components/product-detail.js";
+
+function DetailPage({ isLoading = true, fetchData } = {}) {
+  console.log(fetchData);
+  return /* HTML */ `
+    <div class="min-h-screen bg-gray-50">
+      ${DetailHeader()}
+      <main class="max-w-md mx-auto px-4 py-4">${ProductDetail({ isLoading, fetchData })}</main>
+      ${Footer()}
+    </div>
+  `;
 }
+
+// mount 함수 추가
+DetailPage.mount = function () {
+  const productId = window.location.search.split("productId=")[1];
+  // const store = new DetailStore();
+  // const storeState = store.getState();
+
+  const root = document.getElementById("root");
+  function fetchAndRender() {
+    getProduct(productId).then((data) => {
+      // list 페이지 마운트 후 받아온 데이터로 list 렌더링
+      root.innerHTML = DetailPage({
+        isLoading: false,
+        fetchData: data,
+      });
+    });
+  }
+
+  fetchAndRender();
+};
+
+export default DetailPage;
