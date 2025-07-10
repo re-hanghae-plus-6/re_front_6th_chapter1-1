b7d129b15b34a58578e55aafdd44962eb3234c40
chb6734 <chb6734@gmail.com>
2025-07-11 02:30:30 +0900

    rename: 토스트 타입 이름 수정

--- src/components/common/Toast.js
+++ src/components/common/Toast.js
@@ -10,7 +10,7 @@ export const Toast = (type) => {
       message: "오류가 발생했습니다",
       d: "M6 18L18 6M6 6l12 12",
     },
-    delete: {
+    info: {
       color: "blue",
       message: "선택된 상품들이 삭제되었습니다",
       d: "M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z",
