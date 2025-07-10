210871a1705b78a99b98d849ba77a62f9cb4f76c
sangsoo <wsckm1@gmail.com>
2025-07-10 20:06:33 +0900

    feat: toastState 상태 isOpen:true로 변경 시 toast 컴포넌트 mount

--- src/components/layout/DefaultLayout.js
+++ src/components/layout/DefaultLayout.js
@@ -3,11 +3,14 @@ import Header from './header/Header.js';
 import Footer from './footer/Footer.js';
 import Cart from '../ui/modal/cart/Cart.js';
 import cartStore from '../../store/cartStore.js';
+import toastStore from '../../store/toastStore.js';
+import Toast from '../feedback/toast/Toast.js';
 
 class DefaultLayout extends Component {
   constructor(element, props) {
     super(element, props);
     this.unsubscribeCart = null;
+    this.unsubscribeToast = null;
   }
 
   render() {
@@ -17,11 +20,11 @@ class DefaultLayout extends Component {
         <main id="content"></main>
         <div id="footer"></div>
         <div id="cart"></div>
+        <div id="toast"></div>
       </div>
     `;
     new Header(this.element.querySelector('#header')).mount();
     new Footer(this.element.querySelector('#footer')).mount();
-    new Cart(this.element.querySelector('#cart')).mount();
 
     // children: 페이지 컴포넌트 클래스
     if (this.props && this.props.children) {
@@ -31,9 +34,15 @@ class DefaultLayout extends Component {
     // Cart 표시 로직 분리
     this.updateCart(cartStore.getState());
 
+    // Toast 표시 로직 분리
+    this.updateCart(toastStore.getState());
+
     // 구독 (이미 구독 중이면 중복 방지)
     if (!this.unsubscribeCart) {
       this.unsubscribeCart = cartStore.subscribe(this.updateCart.bind(this));
+    }
+    if (!this.unsubscribeToast) {
+      this.unsubscribeToast = toastStore.subscribe(this.updateToast.bind(this));
     }
   }
 
@@ -47,9 +56,19 @@ class DefaultLayout extends Component {
     // isOpen이 false면 아무것도 표시 X
   }
 
+  updateToast(toastState) {
+    const toastContainer = this.element.querySelector('#toast');
+    if (!toastContainer) return;
+    toastContainer.innerHTML = ''; // 리렌더시 중복 방지
+    if (toastState.isOpen) {
+      new Toast(toastContainer).mount();
+    }
+  }
+
   onUnmount() {
     // 구독 해제 (필수!)
     if (this.unsubscribeCart) this.unsubscribeCart();
+    if (this.unsubscribeToast) this.unsubscribeToast();
   }
 }
 export default DefaultLayout;
