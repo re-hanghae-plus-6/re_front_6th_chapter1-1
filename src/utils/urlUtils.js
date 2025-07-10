39af28624d370c20387c0a9f6b9188a8b6b7cc26
ldhldh07 <dhlim07@naver.com>
2025-07-10 23:09:00 +0900

    feat: 필터 쿼리스트링 연동

--- src/utils/urlUtils.js
+++ src/utils/urlUtils.js
@@ -6,3 +6,24 @@ export const getUrlSegment = (pathname, index) => {
 export const getProductId = (pathname) => {
   return getUrlSegment(pathname, 1);
 };
+
+export const getQueryParams = () => {
+  const params = new URLSearchParams(window.location.search);
+  return {
+    limit: params.get("limit") || "20",
+    sort: params.get("sort") || "price_asc",
+    search: params.get("search") || "",
+  };
+};
+
+export const updateQueryParams = (params) => {
+  const url = new URL(window.location);
+  Object.entries(params).forEach(([key, value]) => {
+    if (value) {
+      url.searchParams.set(key, value);
+    } else {
+      url.searchParams.delete(key);
+    }
+  });
+  window.history.replaceState({}, "", url.toString());
+};
