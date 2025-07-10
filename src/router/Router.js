06b051f3f5f1ffa98c317f2f8b6260a599e49377
Hwirin-Kim <kimhuirin@gmail.com>
2025-07-11 00:13:07 +0900

    feat : router 기능 완성

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,11 +1,14 @@
 import { MainPage } from "../pages/MainPage.js";
 
 export async function Router() {
-  //   if (hash.startsWith("#/product/")) {
-  //     const productId = hash.split("/")[2];
-  //     return ProductDetailPage(productId);
-  //   }
-  // 기본: 메인 페이지
+  const path = window.location.pathname;
+  if (path === "/" || path === "") {
+    return await MainPage();
+  }
+  if (path === "/detail") {
+    // TODO: 상품상세페이지 만들면 꼭 교체하기!
+    return await MainPage();
+  }
 
-  return await MainPage();
+  return "<h1>404 Not Found</h1>";
 }
