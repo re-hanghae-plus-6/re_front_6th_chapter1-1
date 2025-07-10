cbd0d50ef46dbf511adf7f84d8f64a900cfe67fb
chan9yu <dev.cgyeo@gmail.com>
2025-07-10 15:23:06 +0900

    refactor: Header 컴포넌트 재사용성 올리기 위한 개선작업

--- src/pages/ProductListPage.js
+++ src/pages/ProductListPage.js
@@ -158,8 +158,11 @@ export class ProductListPage extends Component {
   render() {
     return /* HTML */ ` //
       <div class="min-h-screen bg-gray-50">
-        ${Header()}
-
+        ${Header({
+          leftContent: /* HTML */ `<h1 class="text-xl font-bold text-gray-900">
+            <a href="/" data-link="">쇼핑몰</a>
+          </h1>`,
+        })}
         <main class="max-w-md mx-auto px-4 py-4">
           <!-- 검색 및 필터 -->
           <div class="bg-white rounded-lg shadow-sm border border-gray-200 p-4 mb-4">
