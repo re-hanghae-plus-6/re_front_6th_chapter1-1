526a9c9ddd9dbf8ae5f6e619ac70fe828352d7ae
현지수 <hyunzsu@naver.com>
2025-07-11 23:40:05 +0900

    feat: 라우트별 페이지 분기 렌더링 구현

--- src/render.js
+++ src/render.js
@@ -1,15 +1,33 @@
 import { ProductListPage } from "./pages/ProductListPage.js";
+import { ProductDetailPage } from "./pages/ProductDetailPage.js";
+import { NotFoundPage } from "./pages/NotFoundPage.js";
 import { registerAllEvents } from "./events.js";
 
-export const createRenderer = (store, productService) => {
+export const createRenderer = (store, productService, router) => {
   const render = () => {
     const state = store.getState();
     const root = document.body.querySelector("#root");
 
-    root.innerHTML = ProductListPage(state);
+    // 라우트별 페이지 분기
+    switch (state.route.name) {
+      case "ProductDetail":
+        root.innerHTML = ProductDetailPage({
+          product: state.productDetail,
+          loading: state.productDetailLoading,
+          relatedProducts: [],
+        });
+        break;
+      case "Cart":
+        root.innerHTML = "<div>장바구니 페이지 (준비중)</div>";
+        break;
+      case "NotFound":
+        root.innerHTML = NotFoundPage(state);
+        break;
+      default:
+        root.innerHTML = ProductListPage(state);
+    }
 
-    // 렌더링 후 이벤트 재등록
-    registerAllEvents(store, productService);
+    registerAllEvents(store, productService, router);
   };
 
   const initRenderer = () => {
