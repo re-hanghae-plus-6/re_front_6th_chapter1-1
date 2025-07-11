e18c938c019b43a454f758067d4a32dba2119b96
bebusl <johndoe@example.com>
2025-07-12 07:40:41 +0900

    fix: 잘못된 페이지 접근시 404

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,8 @@
-import { createElementByString } from "../utils/createViewcomponent";
+import { createElementByString, renderViewComponent } from "../utils/createViewcomponent";
 
 export function NotFoundPage() {
-  return createElementByString(`
+  const component = () =>
+    createElementByString(`
     <div class="text-center my-4 py-20 shadow-md p-6 bg-white rounded-lg">
       <svg viewBox="0 0 320 180" xmlns="http://www.w3.org/2000/svg">
         <defs>
@@ -32,4 +33,6 @@ export function NotFoundPage() {
       
       <a href="/" data-link class="inline-block px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">홈으로</a>
     </div>`);
+
+  renderViewComponent({ component: component }).render();
 }
