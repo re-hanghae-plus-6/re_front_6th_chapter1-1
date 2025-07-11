3f3bc947decb5fe9536dcffdaf11236631b91e8d
youngmin <jym0421@ciel.co.kr>
2025-07-11 15:33:54 +0900

    feat: legacy 분류

---
+++ src/legacy/main.js
@@ -0,0 +1,20 @@
+import routes from "./core/router/routes.js";
+import createRouter from "./core/router/index.js";
+
+const enableMocking = () =>
+  import("./mocks/browser.js").then(({ worker }) =>
+    worker.start({
+      onUnhandledRequest: "bypass",
+    }),
+  );
+
+function main() {
+  createRouter({ routes });
+}
+
+// 애플리케이션 시작
+if (import.meta.env.MODE !== "test") {
+  enableMocking().then(main);
+} else {
+  main();
+}
