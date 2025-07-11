ec43efe4f3d0889ff35827f1cb8e1deb2dc746db
tksemf7410 <tksemf7410@gmail.com>
2025-07-11 18:36:09 +0900

    feat: 상품 상세 페이지 구현 (요구사항 완료)

--- src/pages/productDetailPage.js
+++ src/pages/productDetailPage.js
@@ -4,36 +4,74 @@ import { DetailBreadCrumb } from "../components/detail/layout/detailBreadCrumb.j
 import { Header } from "../components/productList/layout/header.js";
 import { RelatedProducts } from "../components/detail/product/relatedProducts.js";
 import { Footer } from "../components/productList/layout/footer.js";
+import { store } from "../store.js";
+import { getProduct, getProducts } from "../api/productApi.js";
+import { render } from "../render.js";
+import { SuccessToast } from "../components/toast/successToast.js";
+import { router } from "../router.js";
 
 const state = {
   product: null,
-  isLoading: false,
+  isDetailLoading: false,
+  isRelatedProductsLoading: false,
+  isCartAdded: false,
+  relatedProducts: [],
+  cartItems: [],
 };
 
 export const ProductDetailPage = () => {
+  const [mounted, setMounted] = store.useState("mounted");
+
+  const detailPathArray = new URL(window.location.href).pathname.split("/");
+  const productId = detailPathArray[detailPathArray.length - 1];
+
+  if (mounted === false) {
+    state.isDetailLoading = true;
+
+    getProduct(productId).then(async (product) => {
+      state.product = product;
+      state.isDetailLoading = false;
+      setMounted(true);
+      render();
+    });
+  }
+
+  state.isRelatedProductsLoading = true;
+  if (state.product) {
+    new Promise((r) => setTimeout(r, 2_000)).then(() => {
+      getProducts({
+        category1: state.product.category1,
+        category2: state.product.category2,
+      }).then((x) => {
+        state.relatedProducts = x.products.filter((product) => product.productId !== state.product.productId);
+        state.isRelatedProductsLoading = false;
+        render();
+      });
+    });
+  }
+
   return `
     <div class="min-h-screen bg-gray-50">
-      ${Header({ title: "쇼핑몰", cartItemCount: 0 })}
+      ${Header({ title: "상품 상세", cartItemCount: state.cartItems.length })}
     <main class="max-w-md mx-auto px-4 py-4">
       ${
-        state.isLoading
+        state.isDetailLoading
           ? DetailLoadingIndicator
           : `
           <!-- 브레드크럼 -->
           ${DetailBreadCrumb({
-            category1: "생활/건강",
-            category2: "생활용품",
+            category1: state.product.category1,
+            category2: state.product.category2,
           })}
             <!-- 상품 상세 정보 -->
             ${ProductDetail({
-              image: "https://shopping-phinf.pstatic.net/main_8506721/85067212996.1.jpg",
-              name: "PVC 투명 젤리 쇼핑백 1호 와인 답례품 구디백 비닐 손잡이 미니 간식 선물포장",
-              rating: 4.0,
-              reviewCount: 749,
-              price: 220,
-              stock: 107,
-              description:
-                "PVC 투명 젤리 쇼핑백 1호 와인 답례품 구디백 비닐 손잡이 미니 간식 선물포장 브랜드의 우수한 품질을 자랑하는 상품으로, 고객 만족도가 높은 제품입니다.",
+              image: state.product.image,
+              name: state.product.title,
+              rating: state.product.rating,
+              reviewCount: state.product.reviewCount,
+              price: state.product.lprice,
+              stock: state.product.stock,
+              description: state.product.description,
             })}`
       }
       <!-- 상품 목록으로 이동 -->
@@ -44,24 +82,84 @@ export const ProductDetailPage = () => {
         </button>
       </div>
       <!-- 관련 상품 -->
-      ${RelatedProducts({
-        products: [
-          {
-            image: "https://shopping-phinf.pstatic.net/main_8694085/86940857379.1.jpg",
-            name: "샷시 풍지판 창문 바람막이 베란다 문 틈막이 창틀 벌레 차단 샤시 방충망 틈새막이",
-            price: 230,
-            id: "86940857379",
-          },
-          {
-            image: "https://shopping-phinf.pstatic.net/main_8209446/82094468339.4.jpg",
-            name: "실리카겔 50g 습기제거제 제품 /산업 신발 의류 방습제",
-            price: 280,
-            id: "82094468339",
-          },
-        ],
-      })}
+      ${
+        state.isRelatedProductsLoading && (state.relatedProducts ?? []).length <= 0
+          ? ""
+          : RelatedProducts({
+              products: state.relatedProducts,
+            })
+      }
     </main>
     ${Footer}
   </div>
+  ${SuccessToast({ message: "장바구니에 추가되었습니다" })}
   `;
+};
+
+ProductDetailPage.registerEvent = () => {
+  const quantityDecreaseButton = document.querySelector("#quantity-decrease");
+  const quantityIncreaseButton = document.querySelector("#quantity-increase");
+  const quantityInput = document.querySelector("#quantity-input");
+  const addToCartButton = document.querySelector("#add-to-cart-btn");
+  const relatedProductCards = document.querySelectorAll("#related-product");
+
+  if (quantityDecreaseButton) {
+    quantityDecreaseButton.addEventListener("click", () => {
+      const quantity = parseInt(quantityInput.value);
+      if (quantity > 1) {
+        quantityInput.value = quantity - 1;
+      }
+    });
+  }
+
+  if (quantityIncreaseButton) {
+    quantityIncreaseButton.addEventListener("click", () => {
+      const quantity = parseInt(quantityInput.value);
+      if (quantity < state.product.stock) {
+        quantityInput.value = quantity + 1;
+      }
+    });
+  }
+
+  const addToCart = (product, quantity) => {
+    const existingItem = state.cartItems.find((item) => item.productId === product.productId);
+    if (existingItem) {
+      existingItem.quantity += quantity;
+    } else {
+      state.cartItems.push({
+        ...product,
+        quantity: quantity,
+      });
+    }
+  };
+
+  if (addToCartButton) {
+    addToCartButton.addEventListener("click", async () => {
+      const quantity = parseInt(quantityInput.value);
+      addToCart(state.product, quantity);
+
+      setTimeout(() => {
+        const successToast = document.querySelector("#success-toast");
+        if (successToast) {
+          successToast.classList.remove("opacity-0");
+          successToast.classList.add("opacity-100");
+
+          setTimeout(() => {
+            successToast.classList.remove("opacity-100");
+            successToast.classList.add("opacity-0");
+          }, 3000);
+        }
+      }, 0); // render() 이후 DOM이 업데이트된 다음 실행되게 타이밍 조정
+    });
+  }
+
+  if (relatedProductCards) {
+    relatedProductCards.forEach((relatedProductCard) => {
+      relatedProductCard.addEventListener("click", () => {
+        const productId = relatedProductCard.dataset.productId;
+        router.push(`/product/${productId}`);
+        store.setState("mounted", false);
+      });
+    });
+  }
 };
