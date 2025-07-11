f2f061cf93563ebf47ded3399d7d028aa5f73033
현지수 <hyunzsu@naver.com>
2025-07-11 23:40:24 +0900

    feat: 라우트 및 상품 상세 상태 관리 액션 추가

--- src/stores/actions.js
+++ src/stores/actions.js
@@ -4,6 +4,9 @@ export const ACTION_TYPES = {
   SET_LOADING_MORE: "SET_LOADING_MORE",
   APPEND_PRODUCTS: "APPEND_PRODUCTS",
   SET_CATEGORIES: "SET_CATEGORIES",
+  SET_ROUTE: "SET_ROUTE",
+  SET_PRODUCT_DETAIL: "SET_PRODUCT_DETAIL",
+  SET_PRODUCT_DETAIL_LOADING: "SET_PRODUCT_DETAIL_LOADING",
 };
 
 // 액션 생성자 함수들 - 일관된 액션 객체 생성
@@ -31,5 +34,20 @@ export const actions = {
   setCategories: (categories) => ({
     type: ACTION_TYPES.SET_CATEGORIES,
     payload: categories,
+  }),
+
+  setRoute: (route) => ({
+    type: ACTION_TYPES.SET_ROUTE,
+    payload: route,
+  }),
+
+  setProductDetail: (product) => ({
+    type: ACTION_TYPES.SET_PRODUCT_DETAIL,
+    payload: product,
+  }),
+
+  setProductDetailLoading: (isLoading) => ({
+    type: ACTION_TYPES.SET_PRODUCT_DETAIL_LOADING,
+    payload: isLoading,
   }),
 };
