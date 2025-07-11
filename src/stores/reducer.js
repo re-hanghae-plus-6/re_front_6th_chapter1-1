8bef826c3415af5f32fa45440305c27108945e46
현지수 <hyunzsu@naver.com>
2025-07-11 14:49:50 +0900

    feat: 무한스크롤 상태 관리 로직 구현
    - 페이지네이션 정보 관리 (currentPage, hasNext)

--- src/stores/reducer.js
+++ src/stores/reducer.js
@@ -6,6 +6,12 @@ export const initialState = {
   total: 0,
   loading: false,
   categories: {},
+  isLoadingMore: false,
+  pagination: {
+    currentPage: 1,
+    hasNext: true,
+    limit: 20,
+  },
 };
 
 // 순수 함수로 구현된 리듀서
@@ -23,7 +29,32 @@ export const appReducer = (state = initialState, action) => {
         products: action.payload.products,
         total: action.payload.total,
         loading: false,
+        pagination: {
+          ...state.pagination,
+          currentPage: 1,
+          hasNext: action.payload.products.length < action.payload.total,
+        },
       };
+
+    case ACTION_TYPES.SET_LOADING_MORE:
+      return {
+        ...state,
+        isLoadingMore: action.payload,
+      };
+
+    case ACTION_TYPES.APPEND_PRODUCTS: {
+      const newProducts = [...state.products, ...action.payload.products];
+      return {
+        ...state,
+        products: newProducts,
+        pagination: {
+          ...state.pagination,
+          currentPage: action.payload.pagination.page,
+          hasNext: action.payload.pagination.hasNext ?? newProducts.length < state.total,
+        },
+        isLoadingMore: false,
+      };
+    }
 
     case ACTION_TYPES.SET_CATEGORIES:
       return {
