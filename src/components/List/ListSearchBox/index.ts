5c93b6aef05334cc44cba8ce1955064e29746baa
이의찬 <uichan@Seossi.localdomain>
2025-07-12 00:30:19 +0900

    feat: ListSearchBox 구현

--- src/components/List/ListSearchBox/index.ts
+++ src/components/List/ListSearchBox/index.ts
@@ -1,3 +1,4 @@
-import { 상품목록_레이아웃_카테고리_1Depth, 상품목록_레이아웃_카테고리_2Depth } from "./ui";
+import { ListSearchBox } from "./ui";
+import LoadingListSearchBox from "./loadingUi";
 
-export { 상품목록_레이아웃_카테고리_1Depth, 상품목록_레이아웃_카테고리_2Depth };
+export { ListSearchBox, LoadingListSearchBox };
