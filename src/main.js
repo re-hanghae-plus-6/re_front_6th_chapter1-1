daa587ae090c13c6d223f5805c33f04b85006e57
이의찬 <uichan@MacBook-Air-3.local>
2025-07-12 01:19:46 +0900

    feat: main.js에 ListPage 출력

--- src/main.js
+++ src/main.js
@@ -1,20 +1,40 @@
-import Footer from "./shared/ui/footer/index.js";
+import Footer from "./shared/ui/footer";
 import Header from "./components/Header";
+import ListPage from "./pages/ListPage";
+import { getProducts } from "./api/productApi.js";
 
-const enableMocking = () =>
-  import("./mocks/browser.js").then(({ worker }) =>
+const enableMocking = () => {
+  return import("./mocks/browser.js").then(({ worker }) =>
     worker.start({
       onUnhandledRequest: "bypass",
     }),
   );
+};
 
-function main() {
-  document.body.innerHTML = `
+let state = {
+  products: [],
+  total: 0,
+  loading: false,
+};
+
+function render() {
+  document.body.querySelector("#root").innerHTML = `
   <div class="bg-gray-50">
     ${Header}
+    ${ListPage(state)}
     ${Footer}
   </div>
   `;
+}
+
+async function main() {
+  state.loading = true;
+  render();
+  const data = await getProducts({ limit: 20 });
+  state.products = data.products;
+  state.total = data.pagination.total;
+  state.loading = false;
+  render();
 }
 
 // 애플리케이션 시작
