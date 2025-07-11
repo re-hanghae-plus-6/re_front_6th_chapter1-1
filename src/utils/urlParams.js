946300f9bea868e4da8bdf47e666d57ffc4342e9
현지수 <hyunzsu@naver.com>
2025-07-11 11:24:15 +0900

    feat: URL 파싱 함수 유틸 폴더로 분리

---
+++ src/utils/urlParams.js
@@ -0,0 +1,17 @@
+// URL 파라미터 파싱 함수
+export function getURLParams() {
+  const params = new URLSearchParams(window.location.search);
+  return {
+    limit: parseInt(params.get("limit")) || 20,
+    page: parseInt(params.get("page")) || 1,
+  };
+}
+
+// URL 업데이트 함수
+export function updateURLParams(params) {
+  const currentParams = getURLParams();
+  const newParams = { ...currentParams, ...params };
+  const queryString = new URLSearchParams(newParams).toString();
+  const newURL = `${window.location.pathname}?${queryString}`;
+  window.history.pushState({}, "", newURL);
+}
