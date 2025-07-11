3726c526ef36437d1a2fac8eae6f650c244659d2
sangsoo <wsckm1@gmail.com>
2025-07-11 13:52:45 +0900

    feat: toast 유틸 함수 구현

--- src/store/toastStore.js
+++ src/store/toastStore.js
@@ -6,3 +6,32 @@ const toastStore = new CreateStore({
   message: '',
 });
 export default toastStore;
+
+export function openToast({ message, type = 'info', duration = 3000 }) {
+  toastStore.setState({
+    isOpen: true,
+    type,
+    message,
+  });
+
+  // 자동 닫힘
+  if (duration > 0) {
+    setTimeout(() => {
+      closeToast();
+    }, duration);
+  }
+}
+
+export function closeToast() {
+  toastStore.setState({ isOpen: false });
+}
+
+export function toastSuccess(message, duration) {
+  openToast({ message, type: 'success', duration });
+}
+export function toastError(message, duration) {
+  openToast({ message, type: 'error', duration });
+}
+export function toastInfo(message, duration) {
+  openToast({ message, type: 'info', duration });
+}
