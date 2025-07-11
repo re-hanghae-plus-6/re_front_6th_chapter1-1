bafc73e38441e36798ff4b53ceb6da1af71af025
nimusmix <nimusmix@gmail.com>
2025-07-11 18:29:09 +0900

    feat: Component 생성

---
+++ src/core/Component.js
@@ -0,0 +1,35 @@
+export default class Component {
+  $target;
+  state;
+
+  constructor($target) {
+    this.$target = $target;
+    this.setup();
+    this.render();
+  }
+
+  setup() {}
+
+  template() {
+    return "";
+  }
+
+  render() {
+    this.$target.innerHTML = this.template();
+    this.setEvent();
+  }
+
+  setEvent() {}
+
+  setState(newState) {
+    this.state = { ...this.state, ...newState };
+    this.render();
+  }
+
+  addEvent(eventType, selector, callback) {
+    this.$target.addEventListener(eventType, (event) => {
+      if (!event.target.closest(selector)) return false;
+      callback(event);
+    });
+  }
+}
