7cd0cfe62d6bc2ac8cc9d6461715d95d884e84b0
YeongseoYoon <l112xpla@gmail.com>
2025-07-12 00:56:34 +0900

    refactor: 헤더 상태를 명시적으로 관리하여 DOM 조회 제거

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { addEvent } from "./utils/eventManager.js";
 import { ProductListPage } from "./pages/ProductListPage.js";
 import { ProductDetailPage } from "./pages/ProductDetailPage.js";
 import { initCartModal } from "./features/cart/components/CartModal.js";
+import { cartStore } from "./features/cart/store/cartStore.js";
+import { Header } from "./app/components/Header.js";
+import { updateElement } from "./utils/domUtils.js";
 
 const enableMocking = () =>
   import("./mocks/browser.js").then(({ worker, workerOptions }) => worker.start(workerOptions));
@@ -18,12 +21,41 @@ const routes = {
   "/product/:id": ProductDetailPage,
 };
 
+const headerState = {
+  title: "쇼핑몰",
+  showBackButton: false,
+};
+
+const updateHeader = () => {
+  updateElement("header", Header(headerState));
+};
+
+const updateHeaderStateByRoute = () => {
+  const currentPath = router.get().path;
+  if (currentPath.startsWith("/product/")) {
+    headerState.title = "상품 상세";
+    headerState.showBackButton = true;
+  } else {
+    headerState.title = "쇼핑몰";
+    headerState.showBackButton = false;
+  }
+};
+
 function main() {
   router.set(createRouter(routes));
 
   router.get().subscribe(render);
 
+  router.get().subscribe(() => {
+    updateHeaderStateByRoute();
+    updateHeader();
+  });
+
   initCartModal();
+
+  cartStore.subscribe(() => {
+    updateHeader();
+  });
 
   render();
 }
