92dc816e9798e695c97d32852cd8338af1e1c0a0
esoby <esoby104@gmail.com>
2025-07-12 02:27:58 +0900

    feat: 정렬 상태값 로컬 스토리지로 관리

--- src/store.js
+++ src/store.js
@@ -18,25 +18,53 @@ const createStore = (initialState) => {
   return { getState, dispatch, subscribe };
 };
 
-const initialState = {
-  categories: {},
-  products: [],
-  total: 0,
-  loading: false,
-  isFetchingMore: false,
-  hasMore: true,
-  selectedCategory1: null,
-  selectedCategory2: null,
-  cartItemCount: 0,
-  currentLimit: 20,
-  currentSort: "price_asc",
-  currentSearch: "",
-  currentPage: 1,
+const loadStateFromLocalStorage = () => {
+  try {
+    const serializedLimit = localStorage.getItem("currentLimit");
+    const limit = serializedLimit ? parseInt(serializedLimit, 10) : 20;
+
+    const serializedSort = localStorage.getItem("currentSort");
+    const sort = serializedSort || "price_asc";
+
+    return {
+      categories: {},
+      products: [],
+      total: 0,
+      loading: false,
+      isFetchingMore: false,
+      hasMore: true,
+      selectedCategory1: null,
+      selectedCategory2: null,
+      currentLimit: limit,
+      currentSort: sort,
+      currentSearch: "",
+      currentPage: 1,
+    };
+  } catch (error) {
+    console.log("로컬 스토리지 오류 : ", error);
+    return {
+      categories: {},
+      products: [],
+      total: 0,
+      loading: false,
+      isFetchingMore: false,
+      hasMore: true,
+      selectedCategory1: null,
+      selectedCategory2: null,
+      cartItemCount: 0,
+      currentLimit: 20,
+      currentSort: "price_asc",
+      currentSearch: "",
+      currentPage: 1,
+    };
+  }
 };
+
+const initialState = loadStateFromLocalStorage();
 
 export const store = createStore(initialState);
 
 export const resetStoreState = () => {
-  Object.assign(store.getState(), initialState);
-  store.dispatch({ payload: initialState });
+  const resetToState = loadStateFromLocalStorage();
+  store.dispatch({ type: "RESET_STATE", payload: resetToState });
 };
