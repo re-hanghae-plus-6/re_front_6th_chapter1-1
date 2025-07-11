fb1f39166de12f4ba1e6e512edff315dcd1f6596
nimusmix <nimusmix@gmail.com>
2025-07-12 00:13:46 +0900

    style: 레이아웃 변경

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
+import { ToastContainer } from "../common/Toast.js";
 
-export const Layout = (content) => /* html */ `
+export const Layout = (content, navbarOptions = {}) => /* html */ `
 <div class="min-h-screen bg-gray-50">
-  ${Navbar()}
+  ${Navbar({ ...navbarOptions })}
   <main class="max-w-md mx-auto px-4 py-4">${content}</main>
   ${Footer()}
+  ${ToastContainer()}
 </div>
 `;
