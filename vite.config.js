abb299dfd7b7d8210060d66468101cec313be186
김지혜 <ria9810@gmail.com>
2025-07-12 05:04:30 +0900

    chore: 테스트 및 배포 환경 지원 설정 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,17 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/front_6th_chapter1-1/" : "/",
 
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      },
+    },
+  },
   build: {
     outDir: "dist",
     assetsDir: "assets",
