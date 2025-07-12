dede9dca6386ce7fc5511de7224355a0deba657b
이유진 <elli.stdev@gmail.com>
2025-07-12 09:08:40 +0900

    ci: fix ci issue

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,8 @@ export default defineConfig({
       },
     },
   },
-  base: process.env.NODE_ENV === "production" ? "/front_6th_chapter1-1/" : "/",
+
+  base: "/front_6th_chapter1-1/",
 
   build: {
     outDir: "dist",
